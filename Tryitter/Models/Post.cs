3f667d4a50d6ce71c99e161490f0efcc2c83e860
paolofullone <paolo.enrico@gmail.com>
2022-11-25 10:35:24 -0300

    adiciona models categoria, post e user

---
+++ Tryitter/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Tryitter.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
+
+        public DateTime DataPost { get; set; }
+
+        [JsonIgnore]
+        public Categoria? Categoria { get; set; }
+    }
+}
