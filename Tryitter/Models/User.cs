f43420dc7a74b94abb29803ed30345d142f26921
paolofullone <paolo.enrico@gmail.com>
2022-11-25 11:01:46 -0300

    ajuste user model de acordo com requerimentos

--- Tryitter/Models/User.cs
+++ Tryitter/Models/User.cs
@@ -6,9 +6,12 @@ namespace Tryitter.Models
     {
         [Key]
         public int UserId { get; set; }
+        [MinLength(3), MaxLength(30)]
         public string? Name { get; set; }
+        [MinLength(3), MaxLength(30)]
+        public string? Email { get; set; }
         [MinLength(8)]
-        public string? UserName { get; set; }
+        public string? ModuloAtual { get; set; }
         [MinLength(8)]
         public string? Password { get; set; }
     }
